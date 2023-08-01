a8d1a56ccabf9cc292fcdcd41b5fc40d4eb9a016
Cristian Badaracco <cbadaracco@psychcampus.com>
2023-08-01 19:41:41 -0300

    - add millions plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import million from 'million/compiler';
 
 const manifestForPlugin = {
   registerType: "promp",
@@ -41,5 +42,5 @@ const manifestForPlugin = {
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), VitePWA(manifestForPlugin)],
+  plugins: [million.vite({ auto: true }), react(), VitePWA(manifestForPlugin)],
 });
